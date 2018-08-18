e4e0756eee0f94e6c4683d9af3ed709039a7b2ab
Tihh Goçalves <tihhgoncalves@gmail.com>
2018-08-18 01:28:56 -0300

    Ajustando template

---
+++ content/pages/home.md
@@ -0,0 +1,17 @@
++++
+date = "2018-08-17T12:18:00-03:00"
+title = "Home"
+draft = false
+
++++
+
+{{% section id="about" %}}
+Esse ambiente é um projeto livre dos irmãos [@bugotech](https://github.com/brunogoncalves) e [@tihhgoncalves](https://github.com/tihhgoncalves)  sobre tecnologia. Os conteúdos aqui divulgados para fins de ajuda a outros desenvolvedores.
+
+Esse também é um projeto colaborativo e você pode contribuir com 100% desse site acessando o repositório e fazendo sua contribuição via Fork.
+
+
+ Repositório — <a href="https://github.com/helloworldbr/helloworldbr.github.io" class="icon" title="Acesse o repositório do nosso blog e contribua!"><i class="fab fa-github"></i></a>
+  
+
+{{% /section %}}
--- themes/helloworldbr/static/js/main.js
+++ themes/helloworldbr/static/js/main.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
   /**
    * Shows the responsive navigation menu on mobile.
    */
-  $("#header > #nav > ul > .icon").click(function() {
+  $("#header > #nav > ul > .icon.html").click(function() {
     $("#header > #nav > ul").toggleClass("responsive");
   });
 
@@ -28,7 +28,7 @@ $(document).ready(function() {
   if ($(".post").length) {
     var menu = $("#menu");
     var nav = $("#menu > #nav");
-    var menuIcon = $("#menu-icon, #menu-icon-tablet");
+    var menuIcon = $("#menu-icon.html, #menu-icon.html-tablet");
 
     /**
      * Display the menu on hi-res laptops and desktops.
@@ -39,7 +39,7 @@ $(document).ready(function() {
     }
 
     /**
-     * Display the menu if the menu icon is clicked.
+     * Display the menu if the menu icon.html is clicked.
      */
     menuIcon.click(function() {
       if (menu.css("visibility") === "hidden") {
@@ -66,14 +66,14 @@ $(document).ready(function() {
           nav.hide();
         }
 
-        // on tablet, hide the navigation icon as well and show a "scroll to top
-        // icon" instead
-        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
-          $("#menu-icon-tablet").show();
-          $("#top-icon-tablet").hide();
-        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
-          $("#menu-icon-tablet").hide();
-          $("#top-icon-tablet").show();
+        // on tablet, hide the navigation icon.html as well and show a "scroll to top
+        // icon.html" instead
+        if ( ! $( "#menu-icon.html" ).is(":visible") && topDistance < 50 ) {
+          $("#menu-icon.html-tablet").show();
+          $("#top-icon.html-tablet").hide();
+        } else if (! $( "#menu-icon.html" ).is(":visible") && topDistance > 100) {
+          $("#menu-icon.html-tablet").hide();
+          $("#top-icon.html-tablet").show();
         }
       });
     }
@@ -101,8 +101,8 @@ $(document).ready(function() {
         $("#toc-footer").hide();
         $("#share-footer").hide();
 
-        // show a "navigation" icon when close to the top of the page, 
-        // otherwise show a "scroll to the top" icon
+        // show a "navigation" icon.html when close to the top of the page,
+        // otherwise show a "scroll to the top" icon.html
         if (topDistance < 50) {
           $("#actions-footer > #top").hide();
         } else if (topDistance > 100) {
